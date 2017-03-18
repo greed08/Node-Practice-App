18358e019e6c26cb50cd10fe020c982fcf37caef
Omkar8039 <Omilpsablock@gmail.com>
2017-03-18 13:11:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var express=require('express');
+var app=express();
+var message=[
+   "If you are good at something never do it for free",
+   "Power comes from sincere service",
+   "Modesty is the clothing of talent",
+   "Whenever possible,keep it simple."
+
+
+];
+var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+app.set('port',process.env.PORT||3000);
+app.use(express.static(__dirname+'/public'));
+app.get('/',function(req,res)
+{
+	
+	res.render('home');
+});
+app.get('/about',function(req,res)
+{
+	var randomNum=message[Math.floor(Math.random()*message.length)];
+
+	res.render('about',{message:randomNum});
+});
+app.use(function(req,res)
+{
+	//res.type('text/plain');
+	res.status(404);
+	res.render('404');
+});
+app.use(function(err,req,res,next)
+{
+	console.error(err.status);
+	//res.type('text/plain');
+	res.status(500);
+	res.render('500');
+});
+app.listen(app.get('port'),function()
+{
+	console.log('Express started on http://localhost:'+app.get('port')+';press Ctrl+c to terminate.');
+
+});
