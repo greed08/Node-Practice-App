35073c7a3c1c168c5c8ae8b4a14346e0e899a4cd
Omkar8039 <Omilpsablock@gmail.com>
2017-03-20 15:07:29 +0530

    Mongoose learning

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express=require('express');
 var app=express();
+var routes=require('./models/db');
+
 var message=[
    "If you are good at something never do it for free",
    "Power comes from sincere service",
@@ -16,8 +18,9 @@ var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT||3112);
-app.use(require('body-parser')());
+
 app.use(express.static(__dirname+'/public'));
+app.use(require('body-parser')());
 app.get('/',function(req,res)
 {
 	
@@ -27,18 +30,19 @@ app.get('/api',function(req,res)
 {
 	res.json(tours);
 });
-app.get('/registration',function(req,res)
-{
-  res.render('regform', { csrf: 'CSRF token goes here' });
-  
+app.get('/registration', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('regform', { csrf: 'CSRF token goes here' });
 });
-app.post('/process',function(req,res)
-{
-  console.log('Form (form querystring):'+req.query.form);
-  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-console.log('Name (from visible form field): ' + req.body.name);
-console.log('Email (from visible form field): ' + req.body.email);
-res.redirect(303, '/thankyou');
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thankyou');
+    }
 });
 app.get('/thankyou',function(req,res)
 {
